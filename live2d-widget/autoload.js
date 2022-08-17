89cd4ceb825ae03300338baf6460c32f3176ef4b
yuan0038 <949777411@qq.com>
2022-08-17 11:31:55 +0800

    Site updated: 2022-08-17 11:31:54

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://yuansblog.site/live2d-widget/live2d_api/",
+			apiPath: "http://yuansblog.site/live2d-widget/live2d_api/",
 			//cdnPath: "https://yuansblog.site/live2d-widget/live2d_api-1.0.1/"
 			//"https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
