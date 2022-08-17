4e70cc1532050ed35079c62668d6295ef3806ea4
yuan0038 <949777411@qq.com>
2022-08-17 14:49:44 +0800

    Site updated: 2022-08-17 14:49:43

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -215,11 +215,11 @@ function loadWidget(config) {
 			const target = randomSelection(modelList.models[modelId]);
 			loadlive2d("live2d", `${cdnPath}model/${target}/index.json`);
 		} else {
-			if (!modelList) await loadModelList();
-			modelId=1;
-			const target = randomSelection(modelList.models[modelId]);
-			loadlive2d("live2d", `${apiPath}model/${target}/index.json`);
-			//loadlive2d("live2d", `${apiPath}get/?id=${modelId}-${modelTexturesId}`);
+			// if (!modelList) await loadModelList();
+			// modelId=1;
+			// const target = randomSelection(modelList.models[modelId]);
+			// loadlive2d("live2d", `${apiPath}model/${target}/index.json`);
+			loadlive2d("live2d", `${apiPath}get/?id=${modelId}-${modelTexturesId}`);
 			console.log(`Live2D 模型 ${modelId}-${modelTexturesId} 加载完成`);
 		}
 	}
