fdac826b2ab7b223b316cc0479bbbb437f008b2b
yuan0038 <949777411@qq.com>
2022-08-17 11:08:12 +0800

    Site updated: 2022-08-17 11:08:11

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://yuansblog.site/live2d-widget/live2d_api/",
-			cdnPath: "https://yuansblog.site/live2d-widget/live2d_api-1.0.1/"
+			cdnPath: "http://yuansblog.site/live2d-widget/live2d_api-1.0.1/"
 			//"https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
